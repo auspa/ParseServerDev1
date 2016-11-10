fb49ae0c8e34c9cedb25e23464be0acdcc75cd06
A.C <precision.agriculture@yahoo.com>
2016-11-10 11:07:46 +1100

    updated package.json, index.js and main.js for initial checkin

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,15 @@
+var _ = require('underscore');
+var schedule = require('node-schedule');			// https://www.npmjs.com/package/node-schedule
 
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
+var SUPERUSER = process.env.SUPER_USER;
+var SUPERPASSWORD = process.env.SUPER_USER_PASS;
+
+var APP_ID = process.env.APP_ID;
+var MASTER_KEY = process.env.MASTER_KEY;
+var SERVER_URL = process.env.SERVER_URL;
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello World from " + process.env.APP_NAME);
 });
